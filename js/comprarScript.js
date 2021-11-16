44c28b8ca3a32f0ef0d4a40ac3a03caa34d70838
carrizoja <carrizoja@gmail.com>
2021-11-16 16:39:28 -0300

    terceraEntregaV1.1

--- js/comprarScript.js
+++ js/comprarScript.js
@@ -247,3 +247,16 @@ function confirmarCompra() {
 }
 
 // Fin JS para carrito de compras
+
+// Función para el snackbar
+
+function myFunction() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+
+    // Add the "show" class to DIV
+    x.className = "show";
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function() { x.className = x.className.replace("show", ""); }, 3000);
+}
